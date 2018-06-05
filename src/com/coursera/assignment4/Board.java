afdc79d384d8180cf12bdde9de3a94eb456cc3f7
donghyun <egaoneko@naver.com>
2018-06-06 02:22:14 +0900

    Assignment 4
    * Add Board method and test

--- src/com/coursera/assignment4/Board.java
+++ src/com/coursera/assignment4/Board.java
@@ -1,19 +1,30 @@
 package com.coursera.assignment4;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Board {
     private final int[][] blocks;
     private final int n;
-    private int hamming;
 
     public Board(int[][] blocks) { // (where blocks[i][j] = block in row i, column j)
         if (blocks == null) {
             throw new IllegalArgumentException("Can not Board constructor with null");
         }
 
-        this.blocks = Arrays.copyOf(blocks, blocks.length);
         this.n = blocks.length;
+        this.blocks = this.deepCopy(blocks);
+    }
+
+    private int[][] deepCopy(int[][] blocks) {
+        int[][] copy = new int[this.n][this.n];
+
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < this.n; j++) {
+                copy[i][j] = blocks[i][j];
+            }
+        }
+        return copy;
     }
 
     public int dimension() { // board dimension n
@@ -25,7 +36,7 @@ public class Board {
         for (int i = 0; i < this.n; i++) {
             for (int j = 0; j < this.n; j++) {
                 int expected = i * n + j + 1;
-                int actual = blocks[i][j];
+                int actual = this.blocks[i][j];
 
                 if (actual == 0) {
                     continue;
@@ -41,27 +52,124 @@ public class Board {
     }
 
     public int manhattan() { // sum of Manhattan distances between blocks and goal
-        return 0;
+        int manhattan = 0;
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < this.n; j++) {
+                int value = this.blocks[i][j];
+
+                if (value == 0) {
+                    continue;
+                }
+
+                int row = (value - 1) / n;
+                int col = value - (n * row) - 1;
+                manhattan += Math.abs(i - row);
+                manhattan += Math.abs(j - col);
+            }
+        }
+        return manhattan;
     }
 
     public boolean isGoal() { // is this board the goal board?
-        return false;
+        return this.hamming() == 0;
     }
 
     public Board twin() { // a board that is obtained by exchanging any pair of blocks
-        return null;
+        Board twin = new Board(this.deepCopy(this.blocks));
+
+        if (twin.blocks[0][0] != 0 && twin.blocks[0][1] != 0) {
+            twin.swapBlock(0, 0);
+        } else {
+            twin.swapBlock(1, 1);
+        }
+        return twin;
+    }
+
+    private void swapBlock(int r1, int r2) {
+        int temp = this.blocks[r1][0];
+        this.blocks[r1][0] = this.blocks[r2][1];
+        this.blocks[r2][1] = temp;
     }
 
     public boolean equals(Object y) { // does this board equal y?
-        return false;
+        if (y == this) return true;
+        if (y == null || y.getClass() != this.getClass()) return false;
+        if (n != ((Board) y).n) return false;
+
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < this.n; j++) {
+                if (this.blocks[i][j] != ((Board) y).blocks[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public Iterable<Board> neighbors() { // all neighboring boards
-        return null;
+        List<Board> list = new ArrayList<>();
+
+        int[][] blocks = this.deepCopy(this.blocks);
+        int row = -1;
+        int col = -1;
+
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < this.n; j++) {
+                int value = blocks[i][j];
+                if (value != 0) {
+                    continue;
+                }
+                row = i;
+                col = j;
+            }
+        }
+
+        if (row > 0) {
+            exch(blocks, row, col, row - 1, col);
+            list.add(new Board(blocks));
+            exch(blocks, row, col, row - 1, col);
+        }
+
+        if (col > 0) {
+            exch(blocks, row, col, row, col - 1);
+            list.add(new Board(blocks));
+            exch(blocks, row, col, row, col - 1);
+        }
+
+        if (col < n - 1) {
+            exch(blocks, row, col, row, col + 1);
+            list.add(new Board(blocks));
+            exch(blocks, row, col, row, col + 1);;
+        }
+
+        if (row < n - 1) {
+            exch(blocks, row, col, row + 1, col);
+            list.add(new Board(blocks));
+            exch(blocks, row, col, row + 1, col);
+        }
+
+        return list;
+    }
+
+    private void exch(int[][] blocks, int r1, int c1, int r2, int c2) {
+        int temp = blocks[r1][c1];
+        blocks[r1][c1] = blocks[r2][c2];
+        blocks[r2][c2] = temp;
     }
 
     public String toString() { // string representation of this board (in the output format specified below)
-        return null;
+        StringBuilder str = new StringBuilder(this.dimension() + "\n");
+
+        for (int i = 0; i < this.n; i++) {
+            for (int j = 0; j < this.n; j++) {
+                str.append(" ");
+                str.append(this.blocks[i][j]);
+                str.append(" ");
+            }
+            str.append("\n");
+        }
+
+        return str.toString();
     }
 
     public static void main(String[] args) { // unit tests (not graded)
--- src/com/coursera/assignment4/BoardTest.java
+++ src/com/coursera/assignment4/BoardTest.java
@@ -3,8 +3,10 @@ package com.coursera.assignment4;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 public class BoardTest {
 
@@ -56,5 +58,293 @@ public class BoardTest {
                 {4, 5, 6},
                 {7, 0, 8}
         }).hamming(), is(1));
+    }
+
+    @Test
+    public void testManhattan_4() {
+        assertThat(new Board(new int[][]{
+                {0, 1, 3},
+                {4, 2, 5},
+                {7, 8, 6}
+        }).manhattan(), is(4));
+    }
+
+    @Test
+    public void testManhattan_3() {
+        assertThat(new Board(new int[][]{
+                {1, 0, 3},
+                {4, 2, 5},
+                {7, 8, 6}
+        }).manhattan(), is(3));
+    }
+
+    @Test
+    public void testManhattan_5() {
+        assertThat(new Board(new int[][]{
+                {4, 1, 3},
+                {0, 2, 5},
+                {7, 8, 6}
+        }).manhattan(), is(5));
+    }
+
+    @Test
+    public void testIsGoal_true() {
+        assertTrue(new Board(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 0}
+        }).isGoal());
+    }
+
+    @Test
+    public void testIsGoal_false() {
+        assertFalse(new Board(new int[][]{
+                {0, 1, 3},
+                {4, 2, 5},
+                {7, 8, 6}
+        }).isGoal());
+    }
+
+    @Test
+    public void testIsEqual_Equal() {
+        assertEquals(
+                new Board(new int[][]{
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 0}
+                }),
+                new Board(new int[][]{
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 0}
+                })
+        );
+    }
+
+    @Test
+    public void testIsEqual_NotEqual() {
+        assertNotEquals(
+                new Board(new int[][]{
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 0}
+                }),
+                new Board(new int[][]{
+                        {0, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 1}
+                })
+        );
+    }
+
+    @Test
+    public void testTwin_WithoutFirstZero() {
+        Board actual = new Board(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 0}
+        }).twin();
+
+        Board expected = new Board(new int[][]{
+                {2, 1, 3},
+                {4, 5, 6},
+                {7, 8, 0}
+        });
+        assertEquals(
+                actual,
+                expected
+        );
+    }
+
+    @Test
+    public void testTwin_WithFirstZero() {
+        Board actual = new Board(new int[][]{
+                {0, 2, 3},
+                {4, 5, 6},
+                {7, 8, 1}
+        }).twin();
+
+        Board expected = new Board(new int[][]{
+                {0, 2, 3},
+                {5, 4, 6},
+                {7, 8, 1}
+        });
+        assertEquals(
+                actual,
+                expected
+        );
+    }
+
+    @Test
+    public void testNeighbors() {
+        List<Board> neighbors = (List<Board>) new Board(new int[][]{
+                {1, 2, 3},
+                {4, 0, 6},
+                {7, 8, 5}
+        }).neighbors();
+
+        assertThat(neighbors.size(), is(4));
+        assertEquals(
+                neighbors.get(0),
+                new Board(new int[][]{
+                        {1, 0, 3},
+                        {4, 2, 6},
+                        {7, 8, 5}
+                })
+        );
+
+        assertEquals(
+                neighbors.get(1),
+                new Board(new int[][]{
+                        {1, 2, 3},
+                        {0, 4, 6},
+                        {7, 8, 5}
+                })
+        );
+
+        assertEquals(
+                neighbors.get(2),
+                new Board(new int[][]{
+                        {1, 2, 3},
+                        {4, 6, 0},
+                        {7, 8, 5}
+                })
+        );
+
+        assertEquals(
+                neighbors.get(3),
+                new Board(new int[][]{
+                        {1, 2, 3},
+                        {4, 8, 6},
+                        {7, 0, 5}
+                })
+        );
+    }
+
+    @Test
+    public void testNeighbors_LeftTop() {
+        List<Board> neighbors = (List<Board>) new Board(new int[][]{
+                {0, 2, 3},
+                {4, 5, 6},
+                {7, 8, 1}
+        }).neighbors();
+
+        assertThat(neighbors.size(), is(2));
+        assertEquals(
+                neighbors.get(0),
+                new Board(new int[][]{
+                        {2, 0, 3},
+                        {4, 5, 6},
+                        {7, 8, 1}
+                })
+        );
+
+        assertEquals(
+                neighbors.get(1),
+                new Board(new int[][]{
+                        {4, 2, 3},
+                        {0, 5, 6},
+                        {7, 8, 1}
+                })
+        );
+    }
+
+    @Test
+    public void testNeighbors_RightTop() {
+        List<Board> neighbors = (List<Board>) new Board(new int[][]{
+                {1, 2, 0},
+                {4, 5, 6},
+                {7, 8, 3}
+        }).neighbors();
+
+        assertThat(neighbors.size(), is(2));
+        assertEquals(
+                neighbors.get(0),
+                new Board(new int[][]{
+                        {1, 0, 2},
+                        {4, 5, 6},
+                        {7, 8, 3}
+                })
+        );
+
+        assertEquals(
+                neighbors.get(1),
+                new Board(new int[][]{
+                        {1, 2, 6},
+                        {4, 5, 0},
+                        {7, 8, 3}
+                })
+        );
+    }
+
+    @Test
+    public void testNeighbors_LeftBottom() {
+        List<Board> neighbors = (List<Board>) new Board(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {0, 8, 7}
+        }).neighbors();
+
+        assertThat(neighbors.size(), is(2));
+        assertEquals(
+                neighbors.get(0),
+                new Board(new int[][]{
+                        {1, 2, 3},
+                        {0, 5, 6},
+                        {4, 8, 7}
+                })
+        );
+
+        assertEquals(
+                neighbors.get(1),
+                new Board(new int[][]{
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {8, 0, 7}
+                })
+        );
+    }
+
+    @Test
+    public void testNeighbors_RightBottom() {
+        List<Board> neighbors = (List<Board>) new Board(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 0}
+        }).neighbors();
+
+        assertThat(neighbors.size(), is(2));
+        assertEquals(
+                neighbors.get(0),
+                new Board(new int[][]{
+                        {1, 2, 3},
+                        {4, 5, 0},
+                        {7, 8, 6}
+                })
+        );
+
+        assertEquals(
+                neighbors.get(1),
+                new Board(new int[][]{
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 0, 8}
+                })
+        );
+    }
+
+    @Test
+    public void testToString() {
+        String expected = "3\n" +
+                " 0  1  3 \n" +
+                " 4  2  5 \n" +
+                " 7  8  6 \n";
+        String actual = new Board(new int[][]{
+                {0, 1, 3},
+                {4, 2, 5},
+                {7, 8, 6}
+        }).toString();
+
+        assertThat(actual, is(expected));
     }
 }
